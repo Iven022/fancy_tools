07340160d1a5245a8e9e1804311ffbe5722c2ba2
Iven022 <ivenmoonienxd@gmail.com>
2020-04-23 17:07:47 +0400

    Merging dev into master

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 
-dir=$(pwd) #getting current directory of the fancy_tools directory
+dir=$(pwd) #getting current directory pathway of the fancy_tools folder
 
-LINE="source $dir/fancy_functions.sh"
+LINE="source ~/src/fancy_tools/fancy_functions.sh"
 FILE=~/.bashrc
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE" #checking if line exists in .bashrc
 
@@ -13,4 +13,29 @@ temp="$dir/bin"
 if ! [[ $PATH =~ "$temp" ]]; then #checking if path already exists in .bashrc
 	echo "export PATH=$PATH:$dir/bin" >> "$FILE"
 fi
+
+
+
+
+if [[ -d ~/src ]]
+then
+	echo "No need to create the src folder, you already have one."
+else 
+	mkdir ~/src	
+fi
+
+
+
+
+cd ~/src #going to the src folder
+
+
+
+if [[ -d ~/src/fancy_tools ]]
+then
+	echo "No need to clone the fancy tools repository in the src folder, you already have it. Update it from the updateFancyTools script if necessary"
+else 
+	git clone --single-branch --branch master https://github.com/Iven022/fancy_tools.git
+fi
+
 
