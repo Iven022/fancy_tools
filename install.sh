19cfe313f79745228782d03bed03c47b4e8d8876
Iven022 <ivenmoonienxd@gmail.com>
2020-04-23 21:43:54 +0400

    test

--- install.sh
+++ install.sh
@@ -1,34 +1,26 @@
 
-dir=$(pwd) #getting current directory pathway of the fancy_tools folder
-
-LINE="source ~/src/fancy_tools/fancy_functions.sh"
-FILE=~/.bashrc
-grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE" #checking if line exists in .bashrc
-
-LINE2="source $dir/.aliases"
-grep -qF -- "$LINE2" "$FILE" || echo "$LINE2" >> "$FILE"
-
-
-temp="$dir/bin"
-if ! [[ $PATH =~ "$temp" ]]; then #checking if path already exists in .bashrc
-	echo "export PATH=$PATH:$dir/bin" >> "$FILE"
+if [[ -d ~/bin ]] #checking if bin folder exsists
+then
+	echo "No need to create the bin folder, you already have one."
+else 
+	mkdir ~/bin #creating bin folder	
 fi
 
+cd ~/bin
+dir=$(pwd)
+if ! [[ $PATH =~ "$dir" ]]; then #checking if folder is in the path already
+	echo "export PATH=$PATH:$dir" >> "$FILE" #adding the folder to the path
+fi
 
-
-
-if [[ -d ~/src ]]
+if [[ -d ~/src ]] #checking if src folder exists
 then
 	echo "No need to create the src folder, you already have one."
 else 
-	mkdir ~/src	
+	mkdir ~/src 	
 fi
 
 
-
-
 cd ~/src #going to the src folder
-
 
 
 if [[ -d ~/src/fancy_tools ]]
@@ -38,4 +30,14 @@ else
 	git clone --single-branch --branch master https://github.com/Iven022/fancy_tools.git
 fi
 
+LINE="source ~/src/fancy_tools/fancy_functions.sh"
+FILE=~/.bashrc
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE" #checking if line exists in .bashrc
 
+LINE2="source ~/src/fancy_tools/.aliases"
+grep -qF -- "$LINE2" "$FILE" || echo "$LINE2" >> "$FILE"
+
+cp ~/src/fancy_tools/bin/updateFancyTools ~/bin/updateFancyTools #copying the file updatefancytools to the bin folder which is in the path. 
+
+
+echo "Installation completed !"
